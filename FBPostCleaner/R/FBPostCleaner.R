cc0c6d0c3b4b52f66695b5666052177aeec5e82c
matthewluu2002 <156246348+matthewluu2002@users.noreply.github.com>
2024-04-25 22:03:42 -0400

    Add files via upload

---
+++ FBPostCleaner/R/FBPostCleaner.R
@@ -0,0 +1,19 @@
+FBPostCleaner = function(text){
+  text <- gsub("\\p{So}|\\p{Cn}", "", text, perl=TRUE)
+
+  # Replace common abbreviations
+  abbreviations <- c("btw", "lol", "brb", "idk", "omg", "rofl", "bff", "jk", "ily", "ty", "np", "gg", "imo", "imho")
+  full_form <- c("by the way", "laugh out loud", "be right back", "i don't know", "oh my god", "rolling on the floor laughing", "best friends forever", "just kidding", "i love you", "thank you", "no problem", "good game", "in my opinion", "in my humble opinion")
+  for(i in 1:length(abbreviations)){
+    text <- gsub(paste0("\\b", abbreviations[i], "\\b"), full_form[i], text)
+  }
+
+  # Remove Facebook specific features
+  text <- gsub("@\\[\\d+:\\d+\\]", "", text) # Remove Facebook mentions
+  text <- gsub("\\[[^]]+\\]\\(\\/[^)]+\\)", "", text) # Remove Facebook page tags
+
+  # Standard text cleaning
+  text <- gsub("@\\w+|#\\w+|https?://.+|[^\x01-\x7F]|[[:punct:]]|\n|^\\s+|\\s+$|[ |\t]+", " ", text)
+
+  return(text)
+}
